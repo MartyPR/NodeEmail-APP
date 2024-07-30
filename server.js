53d51459d0342776905f067079f6fa4526b2f707
MartyPR <martin.p1131@gmail.com>
2024-07-30 14:21:17 -0500

    add style

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const sendEmail = require("./utils/sendEmail");
 const app = express();
 const PORT = process.env.PORT || 9000;
 
@@ -12,7 +13,16 @@ app.use(express.urlencoded({ extended: false }));
 
 //route to render email form
 app.get("/", (req, res) => {
-  res.render('email-form');
+  res.render("email-form");
+});
+//route to set  the eamil
+app.post("/send-email", async (req, res) => {
+  const { email, message } = req.body;
+  try {
+    sendEmail(email, message);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.listen(PORT, console.log(`The server is running ${PORT}`));
