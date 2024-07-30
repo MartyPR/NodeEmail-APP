3b9f70c065cd3da019579f2198ca21cdfd05f309
MartyPR <martin.p1131@gmail.com>
2024-07-30 11:47:50 -0500

    crear server with express and form to email

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+//!set engine
+app.set("view engine", "ejs");
+//! serrver static assets
+app.use(express.static("public"));
+
+//!pass the data from form;
+app.use(express.urlencoded({extended:false}));
+
+//route to render email form
+app.get('/',(req,res)=>{
+    res.render();
+})
+
+app.listen(PORT, console.log(`The server is running ${PORT}`));
